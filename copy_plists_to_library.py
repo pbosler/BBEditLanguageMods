72d89bee0f5c6e1236c7e009ddd7b264cef542b8
Peter Bosler <pabosle@sandia.gov>
2021-06-24 12:14:54 -0600

    fixed CMake all-caps, upgraded to python3.

--- copy_plists_to_library.py
+++ copy_plists_to_library.py
@@ -3,14 +3,14 @@
 import sys, getopt, os, glob
 
 def print_usage():
-	print 'usage: copy_plists_to_library.py [-t --tw --textwrangler -b --bb --bbedit -h --help -v --version]'
+	print('usage: copy_plists_to_library.py [-t --tw --textwrangler -b --bb --bbedit -h --help -v --version]')
 
 def print_version():
-	print 'BBEditLanguageMods: version 1.0'
-	
+	print('BBEditLanguageMods: version 1.0')
+
 def print_note():
-	print 'NOTE: For these language module changes to take effect, you must restart your text editor.'
-	
+	print('NOTE: For these language module changes to take effect, you must restart your text editor.')
+
 def main(argv):
 	try:
 		opts, args = getopt.getopt(argv, "hvtb", ["tw", "textwrangler", "bb", "bbedit", "help", "version"])
@@ -20,14 +20,14 @@ def main(argv):
 	if len(opts) == 0:
 		print_usage()
 		sys.exit(1)
-	
-	commands = []	
+
+	commands = []
 	for opt, arg in opts:
 		if opt == '-h' or opt == "--help":
 			print_usage()
 			sys.exit()
 		elif opt in ("-t", "--tw", "--textwrangler"):
-			print "TO DO: Find TextWrangler directory."
+			print("TO DO: Find TextWrangler directory.")
 #			for file in glob.glob("*.plist"):
 #				commands.append("cp " + file + " $HOME/Desktop/.")
 		elif opt in ("-b", "--bb", "--bbedit"):
@@ -35,13 +35,13 @@ def main(argv):
 				commands.append("cp " + file + " $HOME/Library/Application\ Support/BBEdit/Language\ Modules/.")
 		elif opt == '-v' or opt == '--version':
 			print_version()
-	
+
 	for cmd in commands:
-		print cmd
+		print(cmd)
 		os.system(cmd)
-	
+
 	print_note()
-		
+
 if __name__ == "__main__":
 	main(sys.argv[1:])
 
